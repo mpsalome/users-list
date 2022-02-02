3fa346de7e78d5c7a9c3095df6811389689da587
Marcus Salomé <marcuspedro7@gmail.com>
2022-02-01 22:33:15 -0300

    Implemented delete user action

--- src/Components/UserAction.js
+++ src/Components/UserAction.js
@@ -1,9 +1,23 @@
 import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
+import { removeUser } from '../store/actions/users';
 
 export const UserAction = ({ action, userId }) => {
+  const executeAction = () => {
+    switch (action.type) {
+      case 'delete':
+        dispatch(removeUser(userId));
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const dispatch = useDispatch();
 
   return (
-    <Button variant={action.variant} size="md" >
+    <Button variant={action.variant} size="md" onClick={executeAction}>
       {action.text}
     </Button>
   );
