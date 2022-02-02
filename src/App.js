e489a9cee0e07b68214addcd8a3b29ae6b005c77
Marcus Salomé <marcuspedro7@gmail.com>
2022-02-01 22:29:24 -0300

    Load users once app renders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ApplicationRoutes } from './router/ApplicationRoutes';
+import { useDispatch } from 'react-redux';
+import { loadUsers } from './store/actions/users';
+import { useEffect } from 'react';
+import { UserService } from './api/UserService';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const getUsers = async () => {
+    const { data } = await UserService.getUsers();
+
+    console.log(data);
+    dispatch(loadUsers(data));
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  
   return (
     <Container>
       <h1>Dashboard</h1>
