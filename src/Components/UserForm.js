78ec5e89c75f443be766d386c706033c63d425d0
Marcus Salomé <marcuspedro7@gmail.com>
2022-02-01 23:49:34 -0300

    Moved form logic into a componet

---
+++ src/Components/UserForm.js
@@ -0,0 +1,162 @@
+import { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../store/actions/users';
+import { useNavigate } from 'react-router-dom';
+
+export const UserForm = ({ user }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    name: '',
+    username: '',
+    email: '',
+    address: {
+      city: '',
+    },
+  });
+  const [errors, setErrors] = useState({});
+
+  
+  useEffect(() => {
+    setData();
+  }, []);
+
+  const setData = () => {
+      if (user) {
+        setFormData(user)
+      }
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+      dispatch(addUser(formData));
+      navigate('/');
+    } 
+  };
+
+  const handleValidation = () => {
+    let fields = formData;
+    let currentErrors = {};
+    let formIsValid = true;
+
+    //Name
+    if (!fields["name"]) {
+      formIsValid = false;
+      currentErrors["name"] = "Cannot be empty";
+    }
+
+    if (fields["name"] !== "") {
+      if (!fields["name"].match(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/)) {
+        formIsValid = false;
+        currentErrors["name"] = "Only letters";
+      }
+    }
+
+    //Email
+    if (!fields["email"]) {
+      formIsValid = false;
+      currentErrors["email"] = "Cannot be empty";
+    }
+
+    if (fields["email"] !== "") {
+      let lastAtPos = fields["email"].lastIndexOf("@");
+      let lastDotPos = fields["email"].lastIndexOf(".");
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          fields["email"].indexOf("@@") === -1 &&
+          lastDotPos > 2 &&
+          fields["email"].length - lastDotPos > 2
+        )
+      ) {
+        formIsValid = false;
+        currentErrors["email"] = "Email is not valid";
+      }
+    }
+
+    //Username
+    if (!fields["username"]) {
+      formIsValid = false;
+      currentErrors["username"] = "Cannot be empty";
+    }
+
+    //City 
+    if (!fields.address["city"]) {
+      formIsValid = false;
+      currentErrors["city"] = "Cannot be empty";
+    }
+
+    setErrors({...currentErrors});
+    return formIsValid;
+  }
+
+
+  return (
+    <div className="wrapper">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            placeholder='Name'
+            type="text"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          />
+        <span className="errorLabel">{errors["name"]}</span>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            placeholder='Username'
+            type="text"
+            value={formData.username}
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+          />
+        <span className="errorLabel">{errors["username"]}</span>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            placeholder='Email address'
+            type="email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+        <span className="errorLabel">{errors["email"]}</span>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="city">
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            placeholder='City'
+            type="text"
+            value={formData.address.city}
+            onChange={(e) =>
+              setFormData({ ...formData, address: { city: e.target.value } })
+            }
+          />
+        <span className="errorLabel">{errors["city"]}</span>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        <LinkContainer to="/">
+          <Button variant="secondary" size="md">
+            Cancel
+          </Button>
+        </LinkContainer>
+      </Form>
+    </div>
+  );
+};
